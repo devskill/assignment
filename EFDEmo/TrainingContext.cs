c8e35109e947c5e9878dd9db2ba56d042806f928
Jalal Uddin <jalaluddin@devskill.com>
2019-05-25 14:00:13 +0600

    added files

---
+++ EFDEmo/TrainingContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EFDEmo
+{
+    public class TrainingContext
+    {
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Course> Courses { get; set; }
+    }
+}
